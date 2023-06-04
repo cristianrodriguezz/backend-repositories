00fce0b63bb53cd585ac1576aac62c80e880c6eb
Cristian Rodriguez <cristiandosespigas@gmail.com>
2023-06-04 11:25:56 -0300

    nuevo commmit

--- controllers/repositories.js
+++ controllers/repositories.js
@@ -1,9 +1,14 @@
 const { repositoriesModel } = require("../models/index");
 
 const getRepositories = async (req, res) => {
-  const data = await repositoriesModel.find({});
-
-  res.send({ data });
+  try {
+    const data = await repositoriesModel.find({});
+    const res = await data;
+    res.send({ data });
+  } catch (error) {
+    console.log(error);
+    console.log("no se pudo hacer esa");
+  }
 };
 const getRepositorie = (req, res) => {};
 const createRepositorie = async (req, res) => {
@@ -14,10 +19,13 @@ const createRepositorie = async (req, res) => {
 };
 const updateRepositorie = async (req, res) => {
   const { body } = req;
-  const {_id: id} = body;
+  const { _id: id } = body;
   console.log(id);
-  const data = await repositoriesModel.updateOne({ _id: id }, { $set: { ...body } });
-  res.send({data})
+  const data = await repositoriesModel.updateOne(
+    { _id: id },
+    { $set: { ...body } }
+  );
+  res.send({ data });
 };
 const deleteRepositorie = (req, res) => {};
 
