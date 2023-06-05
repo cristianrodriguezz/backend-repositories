69efb69d86a2f2ca1ff9d3895ceeceae0d6ea60c
Cristian Rodriguez <cristiandosespigas@gmail.com>
2023-06-05 16:43:32 -0300

    implementando jwt y middleware para los roles

--- config/mongo.js
+++ config/mongo.js
@@ -4,7 +4,7 @@ const dbConnect = () =>{
   const DB_URI = process.env.DB_URI
   try {
     mongoose.connect(
-      "mongodb+srv://cristiandosespigas:CQDrE8xfTjMSO3hX@cluster0.knbokhx.mongodb.net/?retryWrites=true&w=majority",
+      DB_URI,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
