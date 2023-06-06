7ebe11023e1b69ad5f1b54c714749e421cf46e82
Cristian Rodriguez <cristiandosespigas@gmail.com>
2023-06-06 12:58:24 -0300

    cors

--- routes/repositories.js
+++ routes/repositories.js
@@ -4,19 +4,15 @@ const router = express.Router();
 const {validatorCreateItem, validatorGetItem, validatorGetIdBody} = require('../validators/repositories');
 const { authMiddleware } = require("../middleware/session");
 const { checkRol } = require("../middleware/checkRol");
-const cors = require('cors')
 
-var corsOptions = {
-  origin: 'http://example.com',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
 
-router.get("/",cors(corsOptions),getRepositories)
+
+router.get("/",getRepositories)
 
 router.get("/:id",validatorGetItem, getRepositoryById)
 
 router.post("/",authMiddleware,checkRol(['admin','user']),validatorCreateItem,createRepository)
 
-router.put("/",validatorGetIdBody,updateRepository)
+router.put("/",authMiddleware,validatorGetIdBody,updateRepository)
 
 module.exports = router;
